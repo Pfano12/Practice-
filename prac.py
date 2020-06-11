44248c943145f5993fe52bae9e8f14f7f3175dbf
pfano <60345815+Pfano12@users.noreply.github.com>
2020-06-11 12:44:54 +0200

    Quick change

--- prac.py
+++ prac.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 print('Hello pfano')
-=======
-print('Hello pfano, how are u, are u there')
-print('Hi')
->>>>>>> 039d8078b8d70976865699b151366a6ea4adf5f6
+>>> 039d8078b8d70976865699b151366a6ea4adf5f6
+print('I got this')
